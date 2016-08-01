37ca07fcfb15db4d25015370d87a9528eb8b366c
Naoki Yoneyama <naoki.yoneyama.85@gmail.com>
2016-08-01 23:42:49 +0900

    fix isStraight

--- poker.php
+++ poker.php
@@ -81,7 +81,7 @@ class Poker
 	private function isRoyal($cards) {
 		$state = false;
 		$royal = array(1, 10, 11, 12 ,13);
-		if($this->isSameMark($cards)) {
+		if($this->isStraightFlash($cards)) {
 			foreach($cards as $card) {
 				if(in_array($card["number"], $royal)) {
 					$state = true;
@@ -191,6 +191,11 @@ class Poker
 		sort($numbers);
 		$state= true;
 		foreach ($numbers as $number) {
+			if($number == 1) {
+				$last = 9;
+				continue;
+			}
+
 			if ($last !== 0 && $number-$last != 1) {
 				$state = false;
 				break;
